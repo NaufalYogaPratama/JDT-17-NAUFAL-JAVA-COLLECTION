970a736a3776dc76608ab33e59343378725431d2
NaufalYogaPratama <naufaltino2113@gmail.com>
2026-06-02 22:03:40 +0700

    [CHALLENGE] - CLEAN CODE USING JAVA COLLECTION IN EMPLOYEE SERVICE

--- src/main/java/com/indivaragroup/challenge/employee/Employee.java
+++ src/main/java/com/indivaragroup/challenge/employee/Employee.java
@@ -7,6 +7,7 @@ import com.indivaragroup.challenge.employee.service.EmployeeServiceConventional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Employee {
     public static void main(String[] args) {
@@ -16,9 +17,27 @@ public class Employee {
 
         System.out.println("\n========== TEST MODERN SERVICE ==========");
         List<EmployeeDTO> dataFromModernService = SeedDataEmployee.seedingDataEmployee();
-        System.out.println("--- Sort By Name (Modern) ---");
+        System.out.println("\n========== MENGURUTKAN BERDASARKAN NAMA (MODERN) ==========");
         EmployeeService.sortByName(dataFromModernService);
         showData(dataFromModernService);
+
+        System.out.println("\n========== MENGURUTKAN BERDASARKAN ALAMAT (MODERN) ==========");
+        EmployeeService.sortByAddress(dataFromModernService);
+        showData(dataFromModernService);
+
+        System.out.println("\n========== REKAPITULASI KOTA (MODERN)==========");
+        Map<String, Integer> countCityResult = EmployeeService.countCity(dataFromModernService);
+        showCityData(countCityResult);
+
+        System.out.println("\n========== MENGURUTKAN BERDASARKAN GAJI (MODERN) ==========");
+        EmployeeService.sortBySalary(dataFromModernService);
+        showData(dataFromModernService);
+
+        System.out.println("\n========== REKAPITULASI KOTA (MODERN)==========");
+        Map<Character, Integer> countGenderResult = EmployeeService.countGender(dataFromModernService);
+        showGenderData(countGenderResult);
+
+
 
         System.out.println("\n========== TEST CONVENTIONAL SERVICE ==========");
         List<EmployeeDTO> dataFromConventionalService = SeedDataEmployee.seedingDataEmployee();
@@ -50,5 +69,14 @@ public class Employee {
                     employees.getSalary()
             );
         }
+    }
+
+    public static void showCityData(Map<String, Integer> cityMap) {
+        cityMap.forEach((city, amount) -> System.out.println("Kota: " + city + " : " + amount + " pegawai"));
+    }
+
+    public static void showGenderData(Map<Character, Integer> genderMap) {
+        System.out.println("Laki-laki: " + genderMap.getOrDefault('L', 0) + " pegawai");
+        System.out.println("Perempuan: " + genderMap.getOrDefault('P', 0) + " pegawai");
     }
 }
--- src/main/java/com/indivaragroup/challenge/employee/service/EmployeeService.java
+++ src/main/java/com/indivaragroup/challenge/employee/service/EmployeeService.java
@@ -3,7 +3,10 @@ package com.indivaragroup.challenge.employee.service;
 import com.indivaragroup.challenge.employee.dto.EmployeeDTO;
 
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class EmployeeService {
     public static void  sortByName(List<EmployeeDTO> listEmployee) {
@@ -14,7 +17,19 @@ public class EmployeeService {
         listEmployee.sort(Comparator.comparing(EmployeeDTO::getAddress));
     }
 
+    public static Map<String, Integer> countCity(List<EmployeeDTO> listEmployee) {
+        Map<String, Integer> cityMap = new HashMap<>();
+        listEmployee.forEach(employees -> cityMap.merge(employees.getAddress(), 1, Integer::sum));
+        return cityMap;
+    }
+
     public static void sortBySalary(List<EmployeeDTO> listEmployee) {
         listEmployee.sort(Comparator.comparing(EmployeeDTO::getSalary));
+    }
+
+    public static Map<Character, Integer> countGender(List<EmployeeDTO> listEmployee) {
+        Map<Character, Integer> genderMap = new HashMap<>();
+        listEmployee.forEach(employees -> genderMap.merge(employees.getGender(), 1, Integer::sum));
+        return genderMap;
     }
 }
